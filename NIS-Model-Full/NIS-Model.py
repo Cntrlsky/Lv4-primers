b49bc3b0b9acee958a1cd9e075fa08f109489d36
Cntrlsky <hamzamrman@gmail.com>
2026-04-13 14:16:43 +0200

    Removed redundancies

--- NIS-Model-Full/NIS-Model.py
+++ NIS-Model-Full/NIS-Model.py
@@ -5,7 +5,6 @@ from importlib.resources import path
 import numpy as np
 import pandas as pd
 import joblib
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score 
