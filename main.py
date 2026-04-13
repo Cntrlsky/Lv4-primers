ac24543e917e7623152263a9569231c78aa3fd4b
Cntrlsky <hamzamrman@gmail.com>
2026-04-13 14:46:41 +0200

    Made Changes Learned from Test

--- main.py
+++ main.py
@@ -5,7 +5,6 @@
 import numpy as np
 import pandas as pd
 import joblib
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score 
@@ -13,28 +12,34 @@ from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 
 #change the data.csv to the name of your data file. Make sure it is in the same directory as this code.
-data1=pd.read_csv('data.csv')
-
-#Data Encoding
-data2=pd.get_dummies(data1, drop_first=True)
+data01=pd.read_csv("Data")
+data11=pd.read_csv("Data2",)
 
 #Data Duplication 
-data3=data2.drop_duplicates()
+data02=data01.drop_duplicates()
+data12=data11.drop_duplicates()
 
 #Missing Value removel (If too much data is removed use the other value fill meathod (ex:mean,median,mode))
-data=data3.dropna(subset=['Cheating-data'])
+data03=data02.dropna(subset=['class'])
+data13=data12.dropna(subset=['class'])
 
 #X is Input(Training) and  Questions(Test)
 #Y is Output(Training) and Answers(Test)
 #Make sure to change 'Cheating-data' to the name of the column that has the answers in it. This is the column we are trying to predict.
-X = data.drop('Cheating-data', axis=1)
-Y = data['Cheating-data']
+X_train=data13.drop('class',axis=1)
+Y_train=data13['class']
+X_test=data03.drop('class',axis=1)
+Y_test=data03['class']
 
-#This will split the data 70 traning and 30 testing(random state can be any number just keep it consistent for all five models)
-X_train,X_test,Y_train,Y_test=train_test_split(
-    X,Y 
-    ,test_size=0.3
-    ,random_state=2007)
+#Data Encoding (Combine the training and testing data and making it into string)
+X_combined=pd.concat([X_train, X_test], ignore_index=True)
+X_combined=pd.get_dummies(X_combined, drop_first=True)
+X_combined.columns=X_combined.columns.astype(str)
+
+#Split to train and test
+X_train=X_combined.iloc[:len(X_train), :]
+X_test=X_combined.iloc[len(X_train):, :]
+
 
 #this is the model it self i dont belive we will need to change this as random forest is the best for anomaly detection which is the basis of all five of our models
 model=RandomForestClassifier()
@@ -50,4 +55,4 @@ print("This Models Recall Score is", recall_score(Y_test,Predict,average='binary
 print("This Models F1 Score is:", f1_score(Y_test,Predict,average='binary'))
 
 #change the # to the number of the model
-joblib.dump(model, "model#.pkl")
+joblib.dump(model,"model#.pkl")
