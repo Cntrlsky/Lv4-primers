d633e0d77f1c2da7c3f858f3baa968b240704f19
Anasnaggar <anaselnaggar970@gmail.com>
2026-04-14 00:44:13 +0200

    Update Network-Anomaly.py

---
+++ Network-Anomaly-Detection/Network-Anomaly.py
@@ -0,0 +1,48 @@
+#all imports needed for all five models
+import numpy as np
+import pandas as pd
+import joblib
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score 
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+
+#change the data.csv to the name of your data file. Make sure it is in the same directory as this code.
+data01=pd.read_csv("Network-Anomaly-Detection/embedded_system_network_security_dataset.csv")
+
+#Data Duplication 
+data02=data01.drop_duplicates()
+
+#Missing Value removel (If too much data is removed use the other value fill meathod (ex:mean,median,mode))
+data03=data02.dropna(subset=['label'])
+
+
+X=pd.get_dummies(data03.drop(columns=['label']))
+Y=data03['label']
+
+#X is Input(Training) and  Questions(Test)
+#Y is Output(Training) and Answers(Test)
+#Make sure to change 'Cheating-data' to the name of the column that has the answers in it. This is the column we are trying to predict.
+X_train,X_test,Y_train,Y_test=train_test_split(
+    X,Y
+    ,test_size=0.20
+    ,random_state=2007
+)
+
+#this is the model it self i dont belive we will need to change this as random forest is the best for anomaly detection which is the basis of all five of our models
+model=RandomForestClassifier()
+
+#this is the code to train the model
+model.fit(X_train,Y_train)
+
+#this is the code to test the model
+Predict=model.predict(X_test)
+print("This models accuracy Score is",accuracy_score(Y_test,Predict))
+print("This Models Precision Score is", precision_score(Y_test,Predict,average='weighted',zero_division=0))
+print("This Models Recall Score is", recall_score(Y_test,Predict,average='weighted',zero_division=0))
+print("This Models F1 Score is:", f1_score(Y_test,Predict,average='weighted',zero_division=0))
+
+#change the # to the number of the model
+joblib.dump(model,"NtwrkAnmly.pkl")
