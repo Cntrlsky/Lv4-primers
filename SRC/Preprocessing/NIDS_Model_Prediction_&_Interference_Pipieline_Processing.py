20fbf12960c048708c29ff7fc64a2373f6910551
Cntrlsky <236155604+Cntrlsky@users.noreply.github.com>
2026-04-21 02:29:24 +0200

    Added Test data processing pipelines

---
+++ SRC/Preprocessing/NIDS_Model_Prediction_&_Interference_Pipieline_Processing.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+dataTest=pd.read_csv("Data/Processed/NIDS_Model_Test.csv")
+
+X=dataTest.drop(columns=["class"])
+Y=dataTest["class"]
+
+X_PrdIntr,X_useless,Y_PrdIntr,Y_useless=train_test_split(X,
+                                                        Y,
+                                                        test_size=0.6,
+                                                        random_state=2007
+                                                        )
+
+X_PrdIntr.to_csv("Data/Processed/NIDS_PrdIntr_X.csv",index=False)
+Y_PrdIntr.to_csv("Data/Processed/NIDS_PrdIntr_Y.csv",index=False)
