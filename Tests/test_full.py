fbd4d316556ef5ddacea8625d2a3cd81286ee1f0
Cntrlsky <236155604+Cntrlsky@users.noreply.github.com>
2026-04-24 23:02:49 +0300

    New Full test

--- Tests/test_full.py
+++ Tests/test_full.py
@@ -5,22 +5,14 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import pandas as pd
 from SRC.UTILS.Full import Full
 
+
 def test_full_function():
-    X_train=pd.DataFrame({
-        "Data_Limit":[3,5,6,2,3,7,12,22,40,23,31],
-        "Data_Usage":[3,4,3,2,1,7,12,20,35,23,31]
-    })
     
-    X_test=pd.DataFrame({
-        "Data_Limit":[12,55,6,7],
-        "Data_Usage":[12,2,6,6]
-    })
+    X=pd.DataFrame({"Data_Limit":[3,5,6,2,3,7,12,22,40,23,31,12,55,6,7],
+                "Data_Usage":[3,4,3,2,1,7,12,20,35,23,31,12,2,6,6]})
+    Y=pd.Series([1,0,0,1,0,1,1,0,0,1,1,1,0,1,0])
 
-    Y_train=pd.Series([1,0,0,1,0,1,1,0,0,1,1])
-
-    Y_test=pd.Series([1,0,1,0])
-
-    model = Full(X_train,Y_train,X_test,Y_test)
+    model,X_test=Full(X,Y)
 
     assert model is not None
 
